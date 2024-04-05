699e2a77da50dbb51c23f2570095bcb21db8a2eb
mishacap <trustredrum@gmail.com>
2024-04-05 15:03:59 +0300

    fix init place

--- src/Triangle.py
+++ src/Triangle.py
@@ -3,6 +3,7 @@ from Figure import Figure
 
 class Triangle(Figure):
     def __init__(self, side_a: int, side_b: int, side_c: int):
+        super().__init__(name="Triangle")
         if side_a <= 0 or side_b <= 0 or side_c <= 0:
             raise ValueError("Нельзя создать треугольник")
         elif side_a + side_b <= side_c or side_b + side_c <= side_a or side_c + side_a <= side_b:
@@ -10,7 +11,7 @@ class Triangle(Figure):
         self.side_a = side_a
         self.side_b = side_b
         self.side_c = side_c
-        super().__init__(name="Triangle")
+
 
     def get_area(self):
         p = (self.side_a + self.side_b + self.side_c) / 2
