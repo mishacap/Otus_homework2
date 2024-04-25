cf8f5ac3c8d93c836d950c3ef177a2a56c028f98
mishacap <trustredrum@gmail.com>
2024-04-25 18:10:53 +0300

    new tests for figures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,63 @@ import pytest
 
 @pytest.fixture()
 def rectangle_data():
-    side_a, side_b, area = 3, 5, 15
 
-    yield side_a, side_b, area
+    def _wrapper(data: str):
+        if data == "integer":
+            return 3, 5, 15
+        if data == "float":
+            return 3.5, 5.5, 19.25
 
-    print("\n Down")
+    yield _wrapper
+
+
+@pytest.fixture()
+def rectangle_bad_data():
+
+    def _wrapper(data: str):
+        if data == "side_a is 0":
+            return 0, 1
+        if data == "side_b is 0":
+            return 2, 0
+        if data == "side_a less than 0":
+            return -1, 3
+        if data == "side_b less than 0":
+            return 4, -2
+
+    yield _wrapper
+
+
+@pytest.fixture()
+def square_bad_data():
+
+    def _wrapper(data: str):
+        if data == "side is 0":
+            return 0
+        if data == "side less than 0":
+            return -1
+
+    yield _wrapper
+
+
+
+@pytest.fixture()
+def circle_bad_data():
+
+    def _wrapper(data: str):
+        if data == "radius is 0":
+            return 0
+        if data == "radius less than 0":
+            return -1
+
+    yield _wrapper
+
+@pytest.fixture()
+def triangle_bad_data():
+
+    def _wrapper(data: str):
+        if data == "one side is 0":
+            return 0, 5, 6
+        if data == "wrong size of sides":
+            return 3, 4, 10
+
+    yield _wrapper
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,18 +1,69 @@
 from src.Rectangle import Rectangle
 from src.Square import Square
+from src.Circle import Circle
+from src.Triangle import Triangle
 import pytest
 from datetime import datetime
 
 
-def test_rectangle_positive_int():
-    r = Rectangle(3, 5)
-    assert r.get_area() == 15, f"Area should be {3 * 5}"
 
 
-def test_square_positive_int():
-    r = Square(5)
-    assert r.get_area() == 25, f"Area should be {5 * 5}"
+@pytest.mark.parametrize("rectangle_data_1", ["integer", "float"])
+def test_area_rectangle_positive(rectangle_data, rectangle_data_1):
+    side_a, side_b, area = rectangle_data(data=rectangle_data_1)
+    r = Rectangle(side_a, side_b)
+    assert r.get_area() == area, f"Area should be {side_a * side_b}"
+
+
+# def test_rectangle_negative():
+#     with pytest.raises(ValueError):
+#         Rectangle(-3, 5)
+
+@pytest.mark.parametrize('rectangle_bad_data_1', [
+    "side_a is 0",
+    "side_b is 0",
+    "side_a less than 0",
+    "side_b less than 0"])
+def test_rectangle_negative(rectangle_bad_data, rectangle_bad_data_1):
+    side_a, side_b = rectangle_bad_data(data=rectangle_bad_data_1)
+    with pytest.raises(ValueError):
+       Rectangle(side_a, side_b)
+
+
+
+@pytest.mark.parametrize("square_bad_data_1", ["side is 0", "side less than 0"])
+def test_square_negative(square_bad_data, square_bad_data_1):
+    side_a = square_bad_data(data=square_bad_data_1)
+    with pytest.raises(ValueError):
+        Square(side_a)
+
+
+@pytest.mark.parametrize("circle_bad_data_1", ["radius is 0", "radius less than 0"])
+def test_circle_negative(circle_bad_data, circle_bad_data_1):
+    radius = circle_bad_data(data=circle_bad_data_1)
+    with pytest.raises(ValueError):
+        Circle(radius)
+
+
+@pytest.mark.parametrize("triangle_bad_data_1", ["one side is 0", "wrong size of sides"])
+def test_triangle_negative(triangle_bad_data, triangle_bad_data_1):
+    side_a, side_b, side_c = triangle_bad_data(data=triangle_bad_data_1)
+    with pytest.raises(ValueError):
+        Triangle(side_a, side_b, side_c)
+
+
+
+
 
 # def test_rectangle_positive_float():
 #         r = Rectangle(3.5, 5.5)
 #         assert r.get_area() == 19.25, f"Area should be {3.5 * 5.5}"
+
+
+
+
+
+
+# def test_square_positive_int():
+#     r = Square(5)
+#     assert r.get_area() == 25, f"Area should be {5 * 5}"
