4853ed6ecdf246d6fd73647c1b6ec601e01d14fc
mishacap <trustredrum@gmail.com>
2024-04-26 18:11:19 +0300

    new test for rectangle perimeter

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -13,6 +13,12 @@ def test_area_rectangle_positive(rectangle_data_area, rectangle_data_area_1):
     assert r.get_area() == area, f"Area should be {side_a * side_b}"
 
 
+@pytest.mark.parametrize("rectangle_data_perimeter_1", ["integer", "float"])
+def test_perimeter_rectangle_positive(rectangle_data_perimeter, rectangle_data_perimeter_1):
+    side_a, side_b, perimeter = rectangle_data_perimeter(data=rectangle_data_perimeter_1)
+    r = Rectangle(side_a, side_b)
+    assert r.get_perimeter() == perimeter, f"Perimeter should be {2 * side_a + 2 * side_b}"
+
 @pytest.mark.parametrize("square_data_1", ["integer", "float"])
 def test_area_square_positive(square_data, square_data_1):
     side_a, area = square_data(data=square_data_1)
